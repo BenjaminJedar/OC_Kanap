3c4011d1e96baa8bbb4ae902487fa0727fcdece7
Benjamin Jedar <benjisaintpaul@hotmail.fr>
2022-07-28 15:27:11 +0200

    Validation d'ajout des produits dans la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,17 @@
 fetch("http://localhost:3000/api/products")
 
     .then(function (res) {
+        //On récupère la promise et on la convertit au format json
         if (res.ok) {
             return res.json();
         }
     })
     .then(function (product) {
+        /*On crée de nouveau éléments de manière dynamique et on les remplit avec nos données récupérées de l'API*/
         for (let i = 0; i < product.length; i++) {
             let section = document.getElementById("items")
-            section.innerHTML = section.innerHTML + '<a href="./product.html?id=42"><article><img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1"><h3 class="productName">Kanap name1</h3><p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article></a>';
-
-            let h3 = section.querySelector("h3");
-            let img = section.querySelector("img");
-            let p = section.querySelector(".productDescription");
-
-            img.src = product[i].imageUrl;
-            img.alt = product[i].altTxt;
-            h3.textContent = product[i].name;
-            p.textContent = product[i].description;
+            
+            section.innerHTML = section.innerHTML + `<a href="./product.html?id=${product.id}"><article><img src="${product[i].imageUrl}" alt="${product[i].altTxt}"><h3 class="productName">${product[i].name}</h3><p class="productDescription">${product[i].description}</p></article></a>`;
         }
     });
 
