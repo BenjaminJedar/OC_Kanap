3e3572ff84968d83efad70c26dec9aa9d5c4f367
Benjamin Jedar <benjisaintpaul@hotmail.fr>
2022-07-29 15:16:11 +0200

    ajout de la fonction du bouton 'ajouter au panier' qui renvoit au panier + création fichier cart.js

--- front/js/product.js
+++ front/js/product.js
@@ -6,6 +6,7 @@ const title = document.querySelector("#title");
 const description = document.querySelector("#description");
 const colors = document.querySelector("#colors");
 const price = document.querySelector("#price");
+const button = document.querySelector(".item__content__addButton");
 
 fetch("http://localhost:3000/api/products" + "/" + productId)
     .then(function (res) {
@@ -23,4 +24,7 @@ fetch("http://localhost:3000/api/products" + "/" + productId)
         for (let i = 0; i < product.colors.length; i++) {
             colors.innerHTML = colors.innerHTML + `<option value="${product.colors[i]}">${product.colors[i]}</option>`;
         };
-    })
+    });
+
+button.innerHTML = `<a href="./cart.html?id=${productId}"><button id="addToCart">Ajouter au panier</button></a>`;
+
