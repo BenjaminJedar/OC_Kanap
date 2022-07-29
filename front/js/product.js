7f68854e9d0de170b73c710e11309f4a3cb483c5
Benjamin Jedar <benjisaintpaul@hotmail.fr>
2022-07-29 11:57:29 +0200

    Correction du passage de l'id product

--- front/js/product.js
+++ front/js/product.js
@@ -1,13 +1,13 @@
-let urlParam = window.location.href;
-let url = new URL(urlParam);
-let productId = urlParam.searchParams.get("id")
+let url = new URL(window.location.href);
+let productId = url.searchParams.get("id")
 
-const imgProduct = document.querySelector(".item__img");
-const titleProduct = document.querySelector("#title");
-const descriptionProduct = document.querySelector("#description");
-const colorProduct = document.querySelector("#color");
+const img = document.querySelector(".item__img");
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+const color = document.querySelector("#color");
+const price = document.querySelector("#price");
 
-fetch("http://localhost:3000/api/products" + productId)
+fetch("http://localhost:3000/api/products" + productId) 
     .then(function (res) {
         //On récupère la promise et on la convertit au format json
         if (res.ok) {
@@ -15,6 +15,5 @@ fetch("http://localhost:3000/api/products" + productId)
         }
     })
     .then(function (product) {
-        imgProduct.innerHTML = `<img src="${imageUrl}" alt="Photographie d'un canapé">`
-
+        console.log(product[0].name);
     })
--- front/js/script.js
+++ front/js/script.js
@@ -11,7 +11,7 @@ fetch("http://localhost:3000/api/products")
         for (let i = 0; i < product.length; i++) {
             let section = document.getElementById("items")
             
-            section.innerHTML = section.innerHTML + `<a href="./product.html?id=${product.id}"><article><img src="${product[i].imageUrl}" alt="${product[i].altTxt}"><h3 class="productName">${product[i].name}</h3><p class="productDescription">${product[i].description}</p></article></a>`;
+            section.innerHTML = section.innerHTML + `<a href="./product.html?id=${product[i]._id}"><article><img src="${product[i].imageUrl}" alt="${product[i].altTxt}"><h3 class="productName">${product[i].name}</h3><p class="productDescription">${product[i].description}</p></article></a>`;
         }
     });
 
