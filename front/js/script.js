dee181839138fb101b5ad0650ad242cc70dbd04d
Benjamin Jedar <benjisaintpaul@hotmail.fr>
2022-08-01 09:45:36 +0200

    modification de la création des éléments par 'appendChild' et non par 'innerHTML'

--- front/js/script.js
+++ front/js/script.js
@@ -10,8 +10,34 @@ fetch("http://localhost:3000/api/products")
         /*On crée de nouveau éléments de manière dynamique et on les remplit avec nos données récupérées de l'API*/
         for (let i = 0; i < product.length; i++) {
             let section = document.getElementById("items")
-            
-            section.innerHTML = section.innerHTML + `<a href="./product.html?id=${product[i]._id}"><article><img src="${product[i].imageUrl}" alt="${product[i].altTxt}"><h3 class="productName">${product[i].name}</h3><p class="productDescription">${product[i].description}</p></article></a>`;
+
+            //Création du lien principal
+            const productLink = document.createElement("a");
+            section.appendChild(productLink);
+            productLink.href = `../../html/product.html?id=${product[i]._id}`;
+
+            //Création de la balise <article>
+            const productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+            //Création de l'img du produit
+            const productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = product[i].imageUrl;
+            productImg.alt = product[i].altTxt
+
+            //Création d'un titre h3 contenant le nom du produit
+            const productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = product[i].name;
+
+            //Création d'un paragraphe contenant la description du produit
+            const productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productDescription");
+            productDescription.innerHTML = product[i].description;
+
         }
     });
 
